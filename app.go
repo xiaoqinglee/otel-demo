6ae258fefcd72f820c5ffabb8b28ef6e3980874b
lixiaoqing <xiaoqinglee@foxmail.com>
2023-03-07 17:15:22 +0800

    record error

--- app.go
+++ app.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
 	"go.opentelemetry.io/otel/trace"
 	"io"
 	"log"
@@ -51,12 +52,17 @@ func (a *App) Poll(ctx context.Context) (uint, error) {
 
 	var n uint
 	_, err := fmt.Fscanf(a.r, "%d\n", &n)
+	if err != nil {
+		span.RecordError(err)
+		span.SetStatus(codes.Error, err.Error())
+		return 0, err
+	}
 
 	// Store n as a string to not overflow an int64.
 	nStr := strconv.FormatUint(uint64(n), 10)
 	span.SetAttributes(attribute.String("request.n", nStr))
 
-	return n, err
+	return n, nil
 }
 
 // Write writes the n-th Fibonacci number back to the user.
@@ -68,7 +74,12 @@ func (a *App) Write(ctx context.Context, n uint) {
 	f, err := func(ctx context.Context) (uint64, error) {
 		_, span := otel.Tracer(name).Start(ctx, "Fibonacci")
 		defer span.End()
-		return Fibonacci(n)
+		f, err := Fibonacci(n)
+		if err != nil {
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
+		}
+		return f, err
 	}(ctx)
 	if err != nil {
 		a.l.Printf("Fibonacci(%d): %v\n", n, err)
--- fib.go
+++ fib.go
@@ -1,9 +1,16 @@
 package main
 
-// Fibonacci returns the n-th fibonacci number.
+import "fmt"
+
+// Fibonacci returns the n-th fibonacci number. An error is returned if the
+// fibonacci number cannot be represented as a uint64.
 func Fibonacci(n uint) (uint64, error) {
 	if n <= 1 {
 		return uint64(n), nil
+	}
+
+	if n > 93 {
+		return 0, fmt.Errorf("unsupported fibonacci number %d: too large", n)
 	}
 
 	var n2, n1 uint64 = 0, 1
